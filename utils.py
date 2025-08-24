cf1ddd59b5b3da985d1181612025452c1168bab1
JPDLOD <juandelaosssa@gmail.com>
2025-08-23 22:40:28 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
-# Shim para mantener compatibilidad con imports existentes.
-# Reexporta todo lo que está en core_utils.py
+# Reexporta helpers para mantener compatibilidad con imports existentes.
 from core_utils import *  # noqa: F401,F403
