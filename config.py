215efee9042f70730f272e56c610a426061cfd8c
JPDLOD <juandelaosssa@gmail.com>
2025-10-16 20:30:11 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,12 @@ from zoneinfo import ZoneInfo
 
 # OBLIGATORIOS
 BOT_TOKEN = os.environ["BOT_TOKEN"]
-JUSTIFICATIONS_CHAT_ID = int(os.environ["JUSTIFICATIONS_CHAT_ID"])
+
+# ====== CAMBIO PRINCIPAL ======
+# ANTES: JUSTIFICATIONS_CHAT_ID (canal)
+# AHORA: CASES_UPLOADER_ID (usuario que envía casos y justificaciones)
+CASES_UPLOADER_ID = int(os.environ["CASES_UPLOADER_ID"])
+
 FREE_CHANNEL_ID = int(os.environ["FREE_CHANNEL_ID"])
 SUBS_CHANNEL_ID = int(os.environ["SUBS_CHANNEL_ID"])
 ADMIN_USER_IDS = [int(x) for x in os.environ["ADMIN_USER_IDS"].split(",")]
