10d32f36b327d6fbfca73d02a3ded5eba67b3f34
JPDLOD <juandelaosssa@gmail.com>
2025-10-16 20:32:21 -0500

    Delete channel_scanner.py

--- channel_scanner.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import logging
-from channels_handler import cmd_refresh_catalog, process_message_for_catalog
-
-logger = logging.getLogger(__name__)
-
-cmd_replace_caso = None
